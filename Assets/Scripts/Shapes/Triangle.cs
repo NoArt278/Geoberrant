dbcffd08145d1e775dc0ad86a31d7e3e99578fac
Nat10k <110515021+Nat10k@users.noreply.github.com>
2024-03-08 16:29:35 +0700

    Fix triangle

--- Assets/Scripts/Shapes/Triangle.cs
+++ Assets/Scripts/Shapes/Triangle.cs
@@ -9,9 +9,9 @@ public class Triangle : PowerShapes
     {
         SetPoints(new List<Vector2>
         {
-            new Vector2(0, 0.5f),
-            new Vector2(-0.5f, -0.5f),
-            new Vector2(0.5f, -0.5f),
+            new Vector2(0, Mathf.Sqrt(3)),
+            new Vector2(-1f,0),
+            new Vector2(1f, 0),
         });
         SetMass(20);
         SetRollSpeed(300);
