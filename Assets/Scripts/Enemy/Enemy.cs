1e14f26c518bc2fcab6f267d83708d02c1d3f443
Nat10k <noart278@gmail.com>
2024-03-10 22:56:25 +0700

    scientist enemy

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    Rigidbody2D rb;
+    new BoxCollider2D collider;
+    bool isDefeated, seePlayer;
+    float moveSpeed;
+    RaycastHit2D[] hitList;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        collider = GetComponent<BoxCollider2D>();
+        isDefeated = false;
+        seePlayer = false;
+    }
+
+    // Getters and setters
+    public void SetMoveSpeed(float speed)
+    {
+        moveSpeed = speed;
+    }
+    public float GetMoveSpeed()
+    {
+        return moveSpeed;
+    }
+    public bool GetIsDefeated()
+    {
+        return isDefeated;
+    }
+    public bool GetSeePlayer()
+    {
+        return seePlayer;
+    }
+    public Rigidbody2D GetRB()
+    {
+        return rb;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            isDefeated = true;
+            collider.enabled = false;
+            rb.velocity = (collision.relativeVelocity + new Vector2(0, Random.Range(5,20))) * 15;
+            StartCoroutine(DestroySelf());
+        }
+    }
+
+    IEnumerator DestroySelf()
+    {
+        yield return new WaitForSeconds(2f);
+        Destroy(gameObject);
+    }
+
+    private void Update()
+    {
+        if (!seePlayer)
+        {
+            hitList = Physics2D.RaycastAll(transform.position, transform.right, 10f);
+            foreach (RaycastHit2D hit in hitList)
+            {
+                if (hit.collider.CompareTag("Player"))
+                {
+                    seePlayer = true;
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Enemy/Scientist.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scientist : Enemy
+{
+    Coroutine patrol;
+    private void Start()
+    {
+        SetMoveSpeed(10f);
+        patrol = StartCoroutine(Patrol(Mathf.Sign(Random.Range(-1,1))));
+    }
+
+    private void FixedUpdate()
+    {
+        if (GetIsDefeated() && patrol != null)
+        {
+            StopCoroutine(patrol);
+            patrol = null;
+        }
+        if (GetSeePlayer() && !GetIsDefeated())
+        {
+            if (patrol != null)
+            {
+                StopCoroutine(patrol);
+                patrol = null;
+                transform.rotation = Quaternion.Euler(0, (transform.rotation.eulerAngles.y + 180) % 360, 0); // Turn around when see player
+            }
+            GetRB().velocity = new Vector2((transform.rotation.eulerAngles.y != 0 ? -1 : 1) * GetMoveSpeed() * 2, 0);
+        }
+    }
+
+    IEnumerator Patrol(float startDir)
+    {
+        float dir = startDir;
+        if (dir < 0)
+        {
+            transform.rotation = Quaternion.Euler(0, (transform.rotation.eulerAngles.y + 180) % 360, 0);
+        }
+        float prevChangeDirTime = Time.time;
+        while (true)
+        {
+            if (Time.time - prevChangeDirTime >= 3) // Flip direction every 3 seconds
+            {
+                transform.rotation = Quaternion.Euler(0, (transform.rotation.eulerAngles.y + 180) % 360, 0);
+                dir *= -1;
+                prevChangeDirTime = Time.time;
+            }
+            GetRB().velocity = new Vector2(dir * GetMoveSpeed(), 0);
+            yield return null;
+        }
+    }
+}
